7385dfde266e7f1600096faed0dea969819c04d7
groot <99851831+hwang-won@users.noreply.github.com>
2022-03-01 15:56:42 +0900

    JS : Home fade out

--- main.js
+++ main.js
@@ -28,6 +28,17 @@ contactme.addEventListener('click', () => {
     scrollIntoView('#contact');
 });
 
+// home fade out as the window scrolls down
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1.1 - window.scrollY / homeHeight;
+
+});
+
+
+
+// function
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
